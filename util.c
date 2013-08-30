0db5d56c7a0c1e74a481387dd22e0da603be902f
Jonas Fonseca <fonseca@diku.dk>
2013-08-30 08:07:45 -0400

    Move enums and various utility methods to util module

--- util.c
+++ util.c
@@ -57,4 +57,393 @@ die(const char *err, ...)
 	exit(1);
 }
 
+
+DEFINE_ENUM_MAP(author, AUTHOR_ENUM);
+DEFINE_ENUM_MAP(commit_order, COMMIT_ORDER_ENUM);
+DEFINE_ENUM_MAP(date, DATE_ENUM);
+DEFINE_ENUM_MAP(file_size, FILE_SIZE_ENUM);
+DEFINE_ENUM_MAP(filename, FILENAME_ENUM);
+DEFINE_ENUM_MAP(graphic, GRAPHIC_ENUM);
+DEFINE_ENUM_MAP(ignore_space, IGNORE_SPACE_ENUM);
+
+int
+string_enum_compare(const char *str1, const char *str2, int len)
+{
+	size_t i;
+
+#define string_enum_sep(x) ((x) == '-' || (x) == '_' || (x) == '.')
+
+	/* Diff-Header == DIFF_HEADER */
+	for (i = 0; i < len; i++) {
+		if (ascii_toupper(str1[i]) == ascii_toupper(str2[i]))
+			continue;
+
+		if (string_enum_sep(str1[i]) &&
+		    string_enum_sep(str2[i]))
+			continue;
+
+		return str1[i] - str2[i];
+	}
+
+	return 0;
+}
+
+char *
+enum_map_name(const char *name, size_t namelen)
+{
+	static char buf[SIZEOF_STR];
+	int bufpos;
+
+	for (bufpos = 0; bufpos <= namelen; bufpos++) {
+		buf[bufpos] = ascii_tolower(name[bufpos]);
+		if (buf[bufpos] == '_')
+			buf[bufpos] = '-';
+	}
+
+	buf[bufpos] = 0;
+	return buf;
+}
+
+bool
+map_enum_do(const struct enum_map_entry *map, size_t map_size, int *value, const char *name)
+{
+	size_t namelen = strlen(name);
+	int i;
+
+	for (i = 0; i < map_size; i++)
+		if (enum_equals(map[i], name, namelen)) {
+			*value = map[i].value;
+			return TRUE;
+		}
+
+	return FALSE;
+}
+
+
+int
+timecmp(const struct time *t1, const struct time *t2)
+{
+	return t1->sec - t2->sec;
+}
+
+const char *
+mkdate(const struct time *time, enum date date)
+{
+	static char buf[DATE_WIDTH + 1];
+	static const struct enum_map_entry reldate[] = {
+		{ "second", 1,			60 * 2 },
+		{ "minute", 60,			60 * 60 * 2 },
+		{ "hour",   60 * 60,		60 * 60 * 24 * 2 },
+		{ "day",    60 * 60 * 24,	60 * 60 * 24 * 7 * 2 },
+		{ "week",   60 * 60 * 24 * 7,	60 * 60 * 24 * 7 * 5 },
+		{ "month",  60 * 60 * 24 * 30,	60 * 60 * 24 * 365 },
+		{ "year",   60 * 60 * 24 * 365, 0 },
+	};
+	struct tm tm;
+
+	if (!date || !time || !time->sec)
+		return "";
+
+	if (date == DATE_RELATIVE) {
+		struct timeval now;
+		time_t date = time->sec + time->tz;
+		time_t seconds;
+		int i;
+
+		gettimeofday(&now, NULL);
+		seconds = now.tv_sec < date ? date - now.tv_sec : now.tv_sec - date;
+		for (i = 0; i < ARRAY_SIZE(reldate); i++) {
+			if (seconds >= reldate[i].value && reldate[i].value)
+				continue;
+
+			seconds /= reldate[i].namelen;
+			if (!string_format(buf, "%ld %s%s %s",
+					   seconds, reldate[i].name,
+					   seconds > 1 ? "s" : "",
+					   now.tv_sec >= date ? "ago" : "ahead"))
+				break;
+			return buf;
+		}
+	}
+
+	if (date == DATE_LOCAL) {
+		time_t date = time->sec + time->tz;
+		localtime_r(&date, &tm);
+	}
+	else {
+		gmtime_r(&time->sec, &tm);
+	}
+	return strftime(buf, sizeof(buf), DATE_FORMAT, &tm) ? buf : NULL;
+}
+
+const char *
+mkfilesize(unsigned long size, enum file_size format)
+{
+	static char buf[64 + 1];
+	static const char relsize[] = {
+		'B', 'K', 'M', 'G', 'T', 'P'
+	};
+
+	if (!format)
+		return "";
+
+	if (format == FILE_SIZE_UNITS) {
+		const char *fmt = "%.0f%c";
+		double rsize = size;
+		int i;
+
+		for (i = 0; i < ARRAY_SIZE(relsize); i++) {
+			if (rsize > 1024.0 && i + 1 < ARRAY_SIZE(relsize)) {
+				rsize /= 1024;
+				continue;
+			}
+
+			size = rsize * 10;
+			if (size % 10 > 0)
+				fmt = "%.1f%c";
+
+			return string_format(buf, fmt, rsize, relsize[i])
+				? buf : NULL;
+		}
+	}
+
+	return string_format(buf, "%ld", size) ? buf : NULL;
+}
+
+const struct ident unknown_ident = { "Unknown", "unknown@localhost" };
+
+int
+ident_compare(const struct ident *i1, const struct ident *i2)
+{
+	if (!i1 || !i2)
+		return (!!i1) - (!!i2);
+	if (!i1->name || !i2->name)
+		return (!!i1->name) - (!!i2->name);
+	return strcmp(i1->name, i2->name);
+}
+
+/* Parse author lines where the name may be empty:
+ *	author  <email@address.tld> 1138474660 +0100
+ */
+void
+parse_author_line(char *ident, const struct ident **author, struct time *time)
+{
+	char *nameend = strchr(ident, '<');
+	char *emailend = strchr(ident, '>');
+	const char *name, *email = "";
+
+	if (nameend && emailend)
+		*nameend = *emailend = 0;
+	name = chomp_string(ident);
+	if (nameend)
+		email = chomp_string(nameend + 1);
+	if (!*name)
+		name = *email ? email : unknown_ident.name;
+	if (!*email)
+		email = *name ? name : unknown_ident.email;
+
+	*author = get_author(name, email);
+
+	/* Parse epoch and timezone */
+	if (time && emailend && emailend[1] == ' ') {
+		char *secs = emailend + 2;
+		char *zone = strchr(secs, ' ');
+
+		parse_timesec(time, secs);
+
+		if (zone && strlen(zone) == STRING_SIZE(" +0700"))
+			parse_timezone(time, zone + 1);
+	}
+}
+
+static const char *
+get_author_initials(const char *author)
+{
+	static char initials[AUTHOR_WIDTH * 6 + 1];
+	size_t pos = 0;
+	const char *end = strchr(author, '\0');
+
+#define is_initial_sep(c) (isspace(c) || ispunct(c) || (c) == '@' || (c) == '-')
+
+	memset(initials, 0, sizeof(initials));
+	while (author < end) {
+		unsigned char bytes;
+		size_t i;
+
+		while (author < end && is_initial_sep(*author))
+			author++;
+
+		bytes = utf8_char_length(author, end);
+		if (bytes >= sizeof(initials) - 1 - pos)
+			break;
+		while (bytes--) {
+			initials[pos++] = *author++;
+		}
+
+		i = pos;
+		while (author < end && !is_initial_sep(*author)) {
+			bytes = utf8_char_length(author, end);
+			if (bytes >= sizeof(initials) - 1 - i) {
+				while (author < end && !is_initial_sep(*author))
+					author++;
+				break;
+			}
+			while (bytes--) {
+				initials[i++] = *author++;
+			}
+		}
+
+		initials[i++] = 0;
+	}
+
+	return initials;
+}
+
+static const char *
+get_email_user(const char *email)
+{
+	static char user[AUTHOR_WIDTH * 6 + 1];
+	const char *end = strchr(email, '@');
+	int length = end ? end - email : strlen(email);
+
+	string_format(user, "%.*s%c", length, email, 0);
+	return user;
+}
+
+const char *
+mkauthor(const struct ident *ident, int cols, enum author author)
+{
+	bool trim = author_trim(cols);
+	bool abbreviate = author == AUTHOR_ABBREVIATED || !trim;
+
+	if (author == AUTHOR_NO || !ident)
+		return "";
+	if (author == AUTHOR_EMAIL && ident->email)
+		return ident->email;
+	if (author == AUTHOR_EMAIL_USER && ident->email)
+		return get_email_user(ident->email);
+	if (abbreviate && ident->name)
+		return get_author_initials(ident->name);
+	return ident->name;
+}
+
+const char *
+mkmode(mode_t mode)
+{
+	if (S_ISDIR(mode))
+		return "drwxr-xr-x";
+	else if (S_ISLNK(mode))
+		return "lrwxrwxrwx";
+	else if (S_ISGITLINK(mode))
+		return "m---------";
+	else if (S_ISREG(mode) && mode & S_IXUSR)
+		return "-rwxr-xr-x";
+	else if (S_ISREG(mode))
+		return "-rw-r--r--";
+	else
+		return "----------";
+}
+
+DEFINE_ALLOCATOR(realloc_paths, const char *, 256)
+
+const char *
+get_path(const char *path)
+{
+	static const char **paths;
+	static size_t paths_size;
+	int from = 0, to = paths_size - 1;
+	char *entry;
+
+	while (from <= to) {
+		size_t pos = (to + from) / 2;
+		int cmp = strcmp(path, paths[pos]);
+
+		if (!cmp)
+			return paths[pos];
+
+		if (cmp < 0)
+			to = pos - 1;
+		else
+			from = pos + 1;
+	}
+
+	if (!realloc_paths(&paths, paths_size, 1))
+		return NULL;
+	entry = strdup(path);
+	if (!entry)
+		return NULL;
+
+	memmove(paths + from + 1, paths + from, (paths_size - from) * sizeof(*paths));
+	paths[from] = entry;
+	paths_size++;
+
+	return entry;
+}
+
+DEFINE_ALLOCATOR(realloc_authors, struct ident *, 256)
+
+struct ident *
+get_author(const char *name, const char *email)
+{
+	static struct ident **authors;
+	static size_t authors_size;
+	int from = 0, to = authors_size - 1;
+	struct ident *ident;
+
+	while (from <= to) {
+		size_t pos = (to + from) / 2;
+		int cmp = strcmp(name, authors[pos]->name);
+
+		if (!cmp)
+			return authors[pos];
+
+		if (cmp < 0)
+			to = pos - 1;
+		else
+			from = pos + 1;
+	}
+
+	if (!realloc_authors(&authors, authors_size, 1))
+		return NULL;
+	ident = calloc(1, sizeof(*ident));
+	if (!ident)
+		return NULL;
+	ident->name = strdup(name);
+	ident->email = strdup(email);
+	if (!ident->name || !ident->email) {
+		free((void *) ident->name);
+		free(ident);
+		return NULL;
+	}
+
+	memmove(authors + from + 1, authors + from, (authors_size - from) * sizeof(*authors));
+	authors[from] = ident;
+	authors_size++;
+
+	return ident;
+}
+
+void
+parse_timesec(struct time *time, const char *sec)
+{
+	time->sec = (time_t) atol(sec);
+}
+
+void
+parse_timezone(struct time *time, const char *zone)
+{
+	long tz;
+
+	tz  = ('0' - zone[1]) * 60 * 60 * 10;
+	tz += ('0' - zone[2]) * 60 * 60;
+	tz += ('0' - zone[3]) * 60 * 10;
+	tz += ('0' - zone[4]) * 60;
+
+	if (zone[0] == '-')
+		tz = -tz;
+
+	time->tz = tz;
+	time->sec -= tz;
+}
+
 /* vim: set ts=8 sw=8 noexpandtab: */
--- util.h
+++ util.h
@@ -16,6 +16,10 @@
 
 #include "tig.h"
 
+/*
+ * Error handling.
+ */
+
 #define STATUS_CODE_INFO(_) \
 	_(INTEGER_VALUE_OUT_OF_BOUND, "Integer value out of bound"), \
 	_(INVALID_STEP_VALUE, "Invalid step value"), \
@@ -49,6 +53,142 @@ typedef void (*die_fn)(void);
 extern die_fn die_callback;
 void TIG_NORETURN die(const char *err, ...) PRINTF_LIKE(1, 2);
 void warn(const char *msg, ...) PRINTF_LIKE(1, 2);
+
+/*
+ * Enumerations
+ */
+
+struct enum_map_entry {
+	const char *name;
+	int namelen;
+	int value;
+};
+
+struct enum_map {
+	const struct enum_map_entry *entries;
+	const int size;
+};
+
+#define ENUM_MAP_ENTRY(name, value) { name, STRING_SIZE(name), value }
+
+#define ENUM_SYM_MACRO(prefix, name)	prefix##_##name
+#define ENUM_MAP_MACRO(prefix, name)	ENUM_MAP_ENTRY(#name, ENUM_SYM_MACRO(prefix, name))
+
+#define DEFINE_ENUM(name, info) \
+	enum name { info(ENUM_SYM_MACRO) }; \
+	extern const struct enum_map name##_map[]
+
+#define DEFINE_ENUM_MAP(name, info) \
+	static const struct enum_map_entry name##_map_entries[] = { info(ENUM_MAP_MACRO) }; \
+	const struct enum_map name##_map[] = { { name##_map_entries, ARRAY_SIZE(name##_map_entries) } }
+
+int string_enum_compare(const char *str1, const char *str2, int len);
+
+#define enum_equals(entry, str, len) \
+	((entry).namelen == (len) && !string_enum_compare((entry).name, str, len))
+
+char *enum_map_name(const char *name, size_t namelen);
+
+#define enum_name(entry) enum_map_name((entry).name, (entry).namelen)
+
+bool map_enum_do(const struct enum_map_entry *entries, size_t map_size, int *value, const char *name);
+
+#define map_enum(attr, map, name) \
+	map_enum_do(map, ARRAY_SIZE(map), attr, name)
+
+#define AUTHOR_ENUM(_) \
+	_(AUTHOR, NO), \
+	_(AUTHOR, FULL), \
+	_(AUTHOR, ABBREVIATED), \
+	_(AUTHOR, EMAIL), \
+	_(AUTHOR, EMAIL_USER)
+
+DEFINE_ENUM(author, AUTHOR_ENUM);
+
+#define COMMIT_ORDER_ENUM(_) \
+	_(COMMIT_ORDER, DEFAULT), \
+	_(COMMIT_ORDER, TOPO), \
+	_(COMMIT_ORDER, DATE), \
+	_(COMMIT_ORDER, REVERSE)
+
+DEFINE_ENUM(commit_order, COMMIT_ORDER_ENUM);
+
+#define DATE_ENUM(_) \
+	_(DATE, NO), \
+	_(DATE, DEFAULT), \
+	_(DATE, LOCAL), \
+	_(DATE, RELATIVE), \
+	_(DATE, SHORT)
+
+DEFINE_ENUM(date, DATE_ENUM);
+
+#define FILE_SIZE_ENUM(_) \
+	_(FILE_SIZE, NO), \
+	_(FILE_SIZE, DEFAULT), \
+	_(FILE_SIZE, UNITS)
+
+DEFINE_ENUM(file_size, FILE_SIZE_ENUM);
+
+#define FILENAME_ENUM(_) \
+	_(FILENAME, NO), \
+	_(FILENAME, ALWAYS), \
+	_(FILENAME, AUTO)
+
+DEFINE_ENUM(filename, FILENAME_ENUM);
+
+#define GRAPHIC_ENUM(_) \
+	_(GRAPHIC, ASCII), \
+	_(GRAPHIC, DEFAULT), \
+	_(GRAPHIC, UTF_8)
+
+DEFINE_ENUM(graphic, GRAPHIC_ENUM);
+
+#define IGNORE_SPACE_ENUM(_) \
+	_(IGNORE_SPACE, NO), \
+	_(IGNORE_SPACE, ALL), \
+	_(IGNORE_SPACE, SOME), \
+	_(IGNORE_SPACE, AT_EOL)
+
+DEFINE_ENUM(ignore_space, IGNORE_SPACE_ENUM);
+
+/*
+ * Git data formatters and parsers.
+ */
+
+struct time {
+	time_t sec;
+	int tz;
+};
+
+struct ident {
+	const char *name;
+	const char *email;
+};
+
+/* Small cache to reduce memory consumption. It uses binary search to
+ * lookup or find place to position new entries. No entries are ever
+ * freed. */
+const char *get_path(const char *path);
+
+/* Small author cache to reduce memory consumption. It uses binary
+ * search to lookup or find place to position new entries. No entries
+ * are ever freed. */
+struct ident *get_author(const char *name, const char *email);
+
+extern const struct ident unknown_ident;
+#define author_trim(cols) (cols == 0 || cols > 10)
+
+const char *mkdate(const struct time *time, enum date date);
+const char *mkfilesize(unsigned long size, enum file_size format);
+const char *mkauthor(const struct ident *ident, int cols, enum author author);
+const char *mkmode(mode_t mode);
+
+int timecmp(const struct time *t1, const struct time *t2);
+int ident_compare(const struct ident *i1, const struct ident *i2);
+
+void parse_timesec(struct time *time, const char *sec);
+void parse_timezone(struct time *time, const char *zone);
+void parse_author_line(char *ident, const struct ident **author, struct time *time);
 
 #endif
 /* vim: set ts=8 sw=8 noexpandtab: */
